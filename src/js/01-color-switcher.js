6e31975f7b424338c211b197afa09441ae16d88b
Nikita <nicstyle22@gmail.com>
2022-07-29 00:49:51 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('.js-start');
+const stopBtn = document.querySelector('.js-stop');
+let timerId = null;
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.background = `${getRandomHexColor()}`;
+
+    disabledBtn(true, false);
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  disabledBtn(false, true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function disabledBtn(startBoole, stopBoole) {
+  startBtn.disabled = startBoole;
+  stopBtn.disabled = stopBoole;
+}
