f2d8a6216fc6cd652cc561c37cfff94736053d64
Nikita <nicstyle22@gmail.com>
2022-07-31 18:28:47 +0300

    add 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target;
+  const numAmount = Number(amount.value);
+  const numStep = Number(step.value);
+  let numDelay = Number(delay.value);
+  for (let i = 1; i <= numAmount; i++) {
+    createPromise(i, numDelay).then(onSucsess).catch(onError);
+    numDelay += numStep;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSucsess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
